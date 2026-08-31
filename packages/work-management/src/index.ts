d9e589d04bbd30ff0f76ae4ea95f7012c723409c
Nguyễn Trường Sơn <125233768+newsdayads@users.noreply.github.com>
2026-08-31 12:50:52 +0700

    WO-044: make hardened manager the public execution path

--- packages/work-management/src/index.ts
+++ packages/work-management/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types.js';
 export * from './helpers.js';
 export * from './store.js';
-export * from './manager.js';
+export { AutonomousWorkManager as LegacyAutonomousWorkManager } from './manager.js';
+export { AutonomousWorkManager } from './hardened-manager.js';
 export * from './journal-store.js';
 export * from './workforce-bridge.js';
